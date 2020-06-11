73115708a2e67a1b7b410bd7be497d3dd51f8b72
Anthony Oglesby <ogle1ant@gmail.com>
2020-06-11 11:16:04 -0400

    learned PhaserJS lifecycle methods and cordinate systems

--- 01 - Road Crossing Game/00 - Starting/js/game.js
+++ 01 - Road Crossing Game/00 - Starting/js/game.js
@@ -2,13 +2,36 @@
 
 let gameScene = new Phaser.Scene('Game');
 
+// load assets
+
+gameScene.preload = function() {
+  // Load images
+  this.load.image('background', 'assets/background.png');
+  this.load.image('player', 'assets/player.png');
+
+};
+
+// create method
+
+gameScene.create = function() {
+  // create background sprite
+  let bg = this.add.sprite(0, 0, 'background');
+
+  // change the origin of background
+
+  bg.setPosition(640/2,360/2);
+
+  // can grab the width + height from config
+  // let gameW = this.sys.game.config.width
+  // let gameH = this.sys.game.config.height
+};
 
 // set the config of a Game.
 
 let config = {
   type: Phaser.AUTO,
   width: 640,
-  Height: 360,
+  height: 360,
   scene: gameScene
 };
 
